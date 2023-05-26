fc2074167a5c74a04bb97f943a4ace129884ef8a
yoo94 <dbwotjr0329!>
2023-05-26 13:55:56 +0900

    // npm install typescript -global //npm i typescript -g
    // index.ts를 js로 컴파일 하겠다는 의미
    //tsc index.ts

---
+++ getting-started/index.js
@@ -0,0 +1,4 @@
+function sum(a, b) {
+    return a + b;
+}
+var result = sum(10, 20);
---
+++ getting-started/index.ts
@@ -0,0 +1,11 @@
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+var result = sum(10,20);
+
+// npm install typescript -global
+//npm i typescript -g
+
+// index.ts를 js로 컴파일 하겠다는 의미
+//tsc index.ts
