5e6bfb0ebb7c30ade76309e1093e0a2df9c26343
yoo94 <dbwotjr0329!>
2023-05-26 12:56:41 +0900

    js doc 으로 에러를 잡을수 있는 방법
    type 을 선언해놓으면 그 안에 data를 미리 검증하여 에러를 방지할수있다.

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,4 +1,6 @@
 // api url
+// var url = 'https://jsonplaceholder.typicode.com/users';
+// 위에 url 에서 1번 값을 
 var url = 'https://jsonplaceholder.typicode.com/users/1';
 
 // dom
@@ -9,12 +11,46 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} email
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} emil
+ * @property {Address} address
+ * 
+ */
+
+//js doc 기본 문법
+/**
+ * 
+ * @returns {Promise<User>}
+ */
+
+function fetchUser() {
+  return axios.get(url);
+}
+
+fetchUser().then(function (response){
+  response.address
+})
+
+
 function startApp() {
-  axios
-    .get(url)
+  // axios
+  //   .get(url)
+  // 위에랑 같은 로직
+  fetchUser()
     .then(function (response) {
       console.log(response);
       user = response.data;
+      username.innerHTML = user[0].name;
+      email.innerHTML = user[0].email;
+      address.innerHTML = user[0].address.street;
       // TODO: 이름, 이메일, 주소 표시하기
     })
     .catch(function (error) {
