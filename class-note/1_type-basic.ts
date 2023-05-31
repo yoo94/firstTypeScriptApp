855383c1bacc6ef33787d1fc99af0a5d2bc144e6
yoo94 <dbwotjr0329!>
2023-05-31 10:09:15 +0900

    타입스크립트 튜플 객체 진위값

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -11,3 +11,22 @@ let num: number = 10;
 let arr: Array<number> = [1,2,3];
 let heroes: Array<string> = ['dfd','1'];
 let items: number[] = [1,2,3];
+
+//ts 튜플 
+//첫번째는 스트링, 두번째는 숫자를 넣겠다.
+let address: [string,number] = ['gangnam',100];
+
+//ts 객체
+let obj: object = {};
+// let person: object = {
+//     name : 'yoo',
+//     age : 29
+// };
+
+let person:{name : string,age : number} = {
+    name : 'yoo',
+    age : 29
+}
+
+//ts 진위값
+let show: boolean = true;
