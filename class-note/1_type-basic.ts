d2ef64e9fa42ee33e6d2d676910ccc57d97bccca
yoo94 <dbwotjr0329!>
2023-05-31 10:05:22 +0900

    타입스크립트 문자열 숫자 배열

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,13 @@
+//  var str = 'hello';
+
+//ts 문자열
+const str: string = 'hello';
+let str2: string = 'hello';
+
+//ts 숫자
+let num: number = 10;
+
+//ts배열
+let arr: Array<number> = [1,2,3];
+let heroes: Array<string> = ['dfd','1'];
+let items: number[] = [1,2,3];
